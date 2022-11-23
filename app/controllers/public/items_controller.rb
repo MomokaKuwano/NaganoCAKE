623fa0106e43a1f7dc375b1305de4730aa8cd034
EC2 Default User <ec2-user@ip-172-31-15-94.ap-northeast-1.compute.internal>
2022-11-23 12:32:15 +0000

    [finish]public/item/show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
-    @items_count = Item.count
+      @items = Item.where(is_active: true).page(params[:page]).per(8)
+      @items_count = Item.where(is_active: true).count
   end
 
   def show
     @item = Item.find(params[:id])
   end
-  
+
 end
